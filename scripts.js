d7ee3208e48a1b900f54702a50be0295bdcebbd5
singlemutation <164826575+singlemutation@users.noreply.github.com>
2024-04-17 20:07:52 -0400

    Add function that adds equal sized divs to grid container

--- scripts.js
+++ scripts.js
@@ -1,7 +1,17 @@
-const container = document.querySelector("div");
 
-let gridSize = 16;
+const container = document.querySelector(".container");
 
 function makeGrid(gridSize){
     
-}
+    for (let i = 0; i < gridSize; i++){
+        for (let j = 0; j < gridSize; j++){
+            const gridBox = document.createElement("div");
+            gridBox.classList.add("grid-box");
+            container.appendChild(gridBox);
+            gridBox.style.minWidth = 960 / gridSize + "px";
+            gridBox.style.minHeight = 960 / gridSize + "px";
+        };
+    };
+};
+
+makeGrid(16);
